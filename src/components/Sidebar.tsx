0e8d09f08afa4221f89fdbe7546227eb11f73842
Mauro Freire <mauro.freire@cubos.io>
2022-08-30 13:41:56 -0300

    fix: activeButton

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,33 +1,49 @@
-import { FaAward } from 'react-icons/fa';
-import { AiOutlineHome } from 'react-icons/ai';
-import { HiLogout  } from 'react-icons/hi';
+import { FaAward } from "react-icons/fa";
+import { AiOutlineHome } from "react-icons/ai";
+import { HiLogout } from "react-icons/hi";
 
-import styles from '../styles/components/Sidebar.module.css'
-import { signOut } from 'next-auth/client';
-import Link from 'next/link';
+import styles from "../styles/components/Sidebar.module.css";
+import { signOut } from "next-auth/client";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Sidebar() {
+  const router = useRouter();
 
-    return (
-        <aside className={styles.container}>
-            <section className={styles.logo}>
-                <img src="./logo-sidebar.svg" alt="logo-sidebar"/>
-            </section>
-            <section>
-                <nav className={styles.containerNavigation}>
-                    <Link href="/">
-                        <a className={styles.navigationButton}><AiOutlineHome size={35}/></a>
-                    </Link>
-                    <Link href="/leaderboard">
-                    <a className={styles.navigationButton}><FaAward size={35}/></a>
-                    </Link>
-                </nav>
-            </section>
-            <section className={styles.logout}>
-                <button onClick={() => signOut()} className={styles.logoutButton}>
-                    <HiLogout />
-                </button>
-            </section>
-        </aside>
-    );
+  const currentRouter = router.pathname;
+
+  return (
+    <aside className={styles.container}>
+      <section className={styles.logo}>
+        <img src="./logo-sidebar.svg" alt="logo-sidebar" />
+      </section>
+      <section>
+        <nav className={styles.containerNavigation}>
+          <Link href="/">
+            <a
+              className={`${styles.navigationButton} ${
+                currentRouter == "/" && styles.navigationButtonActive
+              }`}
+            >
+              <AiOutlineHome size={35} />
+            </a>
+          </Link>
+          <Link href="/leaderboard">
+            <a
+              className={`${styles.navigationButton} ${
+                currentRouter == "/leaderboard" && styles.navigationButtonActive
+              }`}
+            >
+              <FaAward size={35} />
+            </a>
+          </Link>
+        </nav>
+      </section>
+      <section className={styles.logout}>
+        <button onClick={() => signOut()} className={styles.logoutButton}>
+          <HiLogout />
+        </button>
+      </section>
+    </aside>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,17 @@
-import '../styles/global.css'
-import styles from '../styles/global.module.css'
-import { Provider } from "next-auth/client"
-import { Sidebar } from '../components/Sidebar'
+import "../styles/global.css";
+import styles from "../styles/global.module.css";
+import { Provider } from "next-auth/client";
+import { Sidebar } from "../components/Sidebar";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <div className={styles.container}>
       <Provider session={pageProps.session}>
-        {router.pathname != "/login" && (
-          <Sidebar/>
-        )
-        }
+        {router.pathname != "/login" && <Sidebar />}
         <Component {...pageProps} />
       </Provider>
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
